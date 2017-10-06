5278e484d5d50ca108c22df7cd3d319d936ba50d
Matt Spinnelli <matt.spinelli@cordatahealth.com>
2017-10-06 11:56:53 -0400

    encription scripts needed some fault tolerance and progress on hw03

--- decrypt_hw.sh
+++ decrypt_hw.sh
@@ -1,4 +1,6 @@
 gpg-zip --output homework/my_solutions.tar --decrypt homework/my_solutions.tar.gpg
-rm -R homework/my_solutions.tar.gpg
-tar -xf homework/my_solutions.tar
-rm -R homework/my_solutions.tar
+if [ $? == 0 ]; then
+  rm -R homework/my_solutions.tar.gpg
+  tar -xf homework/my_solutions.tar
+  rm -R homework/my_solutions.tar
+fi
--- encrypt_hw.sh
+++ encrypt_hw.sh
@@ -1,2 +1,5 @@
 gpg-zip --output homework/my_solutions.tar.gpg --symmetric homework/my_solutions
-rm -R homework/my_solutions/
+if [ $? == 0 ]; then
+  rm -R ~/Documents/my_solutions
+  mv homework/my_solutions/ ~/Documents/my_solutions
+fi
